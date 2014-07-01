15e25d71f757debd140dc5bd7e785e3a0ab1d6c2
Martin M Sheeks <martin.sheeks@gmail.com>
2014-07-01 12:45:12 -0500

    Setup
    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<script src="components/platform/platform.js"></script>
+		<link rel="import" href="components/boot-strapped/bootstrap-page.html">
+		<link rel="import" href="components/boot-strapped/nav-bar.html">
+	</head>
+	<body>
+		<bootstrap-page>
+			<nav-bar inverse="true"></nav-bar>
+		</bootstrap-page>
+	</body>
+</html>
